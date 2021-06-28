b1dfb589a60e259529514a321324cdf9b2dfeaf2
Martyna <martyna.sowinska@hotmail.com>
2021-06-28 10:45:33 +0200

    JS Basics
    Solved

--- solo_project.js
+++ solo_project.js
@@ -16,29 +16,56 @@
    Create a variable called "test" and assign a string to it.
 */
 
+let test = "string"
+
 /* Ex.B
     Create a variable called "sum" and assign to it the result of the sum between 10 and 20.
 */
+
+let sum = 10 + 20
 
 /* Ex.C 
     Create a variable called "random" and assign to it a random number between 0 and 20 (it should be randomly created at each execution).
 */
 
+let random =  Math.floor(Math.random() * 20)
+// console.log(random)
+
+
 /* Ex.D
     Create a variable called "me" and assign to it an object containing the following information: name = your name, surname = your surname, age = your age.
 */
+
+let me = {
+  name: "Martyna",
+  surname: "Sowińska",
+  age: 21,
+}
 
 /* Ex.E 
     Programmatically remove the age property from the previously create object.
 */
 
+delete me.age
+
 /* Ex.F 
    Programmatically add to the object me an array called "skills", containing the programming languages you know right now.
 */
 
+me.skills = ["Python", "JavaScript", "C++"]
+// console.log(me)
+
 /* Ex.G 
    Programmatically remove the last skill from the "skills" array inside the "me" object.
 */
+
+// 1.
+// me.skills.length = 2
+
+//2.
+let beGone = me.skills.pop()
+console.log(me)
+
 
 // JS Functions
 /* Ex.1
